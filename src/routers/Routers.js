8f17e99eb0ae68b4d1c613ddc18cba99b004dcf7
Ismahenabdallah <abdallah.ismahen99@gmail.com>
2022-12-07 15:15:53 +0100

    first commit

---
+++ src/routers/Routers.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import Blog from "../pages/Blog";
+import BlogDetails from "../pages/BlogDetails";
+import NotFound from "../pages/NotFound";
+import Contact from "../pages/Contact";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+ 
+     
+      <Route path="/blogs" element={<Blog />} />
+      <Route path="/blogs/:slug" element={<BlogDetails />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default Routers;
